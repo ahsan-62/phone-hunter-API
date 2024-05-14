9f606e94c8e364351f0b63dd24ae4700cc31ba51
ahsan-62 <ahsan.cse.101792@gmail.com>
2024-05-14 20:09:18 +0600

    Modal Show with Js

--- script/phone.js
+++ script/phone.js
@@ -1,13 +1,13 @@
 
 
 
-const loadPhone = async (searchText,isShowAll) => {
+const loadPhone = async (searchText='iphone',isShowAll) => {
 
     const res = await fetch ( `https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     const phone=data.data;
 
-    console.log(isShowAll);
+    // console.log(isShowAll);
     const showAll=document.getElementById('show-all');
     if(phone.length>10 ){
         showAll.classList.remove('hidden');
@@ -46,7 +46,7 @@ const loadPhone = async (searchText,isShowAll) => {
             <h2 class="card-title">${phone.phone_name}</h2>
             <p>If a dog chews shoes whose shoes does he choose?</p>
             <div class="card-actions">
-            <button class="btn btn-primary">Buy Now</button>
+            <button onclick="showDetails('${phone.slug}')" class="btn btn-primary">Show Details</button>
             </div>
             </div>
             `
@@ -84,3 +84,37 @@ const loadPhone = async (searchText,isShowAll) => {
                 
             searchPhone(true);
          }
+
+         const showDetails= async (id)=>{
+
+            const res= await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+            const phoneDetails = await res.json();
+           const data= phoneDetails.data;
+
+            displayDetails(phoneDetails.data);
+         }
+
+         const displayDetails= (phoneData)=>{
+
+            console.log(phoneData);
+            details_modal.showModal();
+            const modalDetails=document.getElementById('modal-details');
+
+            modalDetails.innerHTML=`
+            <h3 class="text-2xl font-bold   " >${phoneData.name}</h3>
+
+            <img class="w-1/2" src="${phoneData.image}">
+
+            <h2>Brand: ${phoneData.brand}</h2>
+            
+            <p>Release Date: ${phoneData.releaseDate?phoneData.releaseDate:'No Release Date'}</p>
+            <p>Display Size: ${phoneData.mainFeatures.displaySize?phoneData.mainFeatures.displaySize:'No Display Size'}</p>
+            <p>Memory: ${phoneData.mainFeatures.memory?phoneData.mainFeatures.memory:'No Memory'}</p>
+            <p>Storage: ${phoneData.mainFeatures.storage?phoneData.mainFeatures.storage:'No Storage'}</p>
+            <p>Sensors: ${phoneData.mainFeatures.sensors?phoneData.mainFeatures.sensors:'No Sensors'}</p>
+            `
+            
+         }
+         
+
+         loadPhone();
